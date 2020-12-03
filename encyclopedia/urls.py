dcb9ee3703f74396321ae2ae68b03133c44782bd
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-12-03 20:33:01 +0700

    Add path for newentry in urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry_title>", views.entry, name="entry"),
-    path("wiki/<str:edit_entry>/edit", views.edit, name="edit")
+    path("wiki/<str:edit_entry>/edit", views.edit, name="edit"),
+    path("newentry", views.newentry, name="newentry")
 ]
